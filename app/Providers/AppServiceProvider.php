2e39c9e3439510708dc8c91d0983273ed02c8975
DESKTOP-DG231KI\autul <moshiur.autul@orbitax.com>
2022-03-05 17:29:13 +0600

    dependency problem fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
